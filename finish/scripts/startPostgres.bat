e71e73d7555cd8511879e78087cc7dc3b22c8458
Gilbert Kwan <gkwan@ca.ibm.com>
2024-06-03 12:13:19 -0400

    refactor the app
    Signed-off-by: Gilbert Kwan <gkwan@ca.ibm.com>

--- finish/scripts/startPostgres.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-docker ps | findStr "postgres-sample" >NUL && echo Postgres is running && exit /B 0
-
-cd .\postgres || exit
-docker build -t postgres-sample .
-docker run -d -p 5432:5432 --rm --name postgres-container postgres-sample --max_prepared_transactions=100
-cd ..\..\
--- finish/scripts/stopPostgres.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-
-docker stop postgres-container
