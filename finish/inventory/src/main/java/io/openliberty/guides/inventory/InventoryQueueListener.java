ffa6d2b238ac85d6e68fd06bd019f6c408ab3b66
Siwan Yang <syang00129@gmail.com>
2024-06-05 17:31:21 -0400

    update listener

--- finish/inventory/src/main/java/io/openliberty/guides/inventory/InventoryQueueListener.java
+++ finish/inventory/src/main/java/io/openliberty/guides/inventory/InventoryQueueListener.java
@@ -2,6 +2,7 @@ package io.openliberty.guides.inventory;
 
 import io.openliberty.guides.models.SystemLoad;
 import jakarta.ejb.MessageDriven;
+import jakarta.inject.Inject;
 import jakarta.jms.JMSException;
 import jakarta.jms.Message;
 import jakarta.jms.MessageListener;
@@ -12,7 +13,8 @@ import java.util.logging.Logger;
 @MessageDriven(mappedName="jms/InventoryQueue")
 public class InventoryQueueListener implements MessageListener {
     private static Logger logger = Logger.getLogger(InventoryQueueListener.class.getName());
-    private InventoryManager manager = new InventoryManager();
+    @Inject
+    private InventoryManager manager;
 
     @Override
     public void onMessage(Message message) {
