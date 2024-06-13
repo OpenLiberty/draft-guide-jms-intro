525d08408dbb8aef405e4841289274500ddd0da0
Siwan Yang <123605713+siwany@users.noreply.github.com>
2024-06-13 11:47:28 -0400

    Update test.sh/test.yml (#7)
    * add listener
    
    * update listener
    
    * inventory listener update
    
    * edit minor part
    
    * test case update
    
    * update inventory test 2
    
    * 3 test cases
    
    * EndpointIT
    
    * test.sh
    
    * update test.sh
    
    * testApp.sh
    
    * testApp.sh final
    
    * delete scripts
    
    * test.yml updates

--- finish/scripts/packageApps.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-mvn -ntp -Dhttp.keepAlive=false \
-    -Dmaven.wagon.http.pool=false \
-    -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
-    -pl models clean install
-mvn -ntp -Dhttp.keepAlive=false \
-    -Dmaven.wagon.http.pool=false \
-    -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
-    -q clean package
--- finish/scripts/stopContainers.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker stop command query inventory postgres-container
-
-docker network rm cqrs-app
-
-docker ps
-
-docker container ls -a
