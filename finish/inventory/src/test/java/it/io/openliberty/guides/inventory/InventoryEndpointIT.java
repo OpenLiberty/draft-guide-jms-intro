e85297903df2b2d2bd45252a3132e2b82015e391
Siwan Yang <syang00129@gmail.com>
2024-06-13 13:02:27 -0400

    Assetions re update

--- finish/inventory/src/test/java/it/io/openliberty/guides/inventory/InventoryEndpointIT.java
+++ finish/inventory/src/test/java/it/io/openliberty/guides/inventory/InventoryEndpointIT.java
@@ -28,9 +28,10 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.TestMethodOrder;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
