5a25c225856c903e3002e297bd1626e57a35e95b
Siwan Yang <syang00129@gmail.com>
2024-06-13 10:40:05 -0400

    testApp.sh final

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -1,32 +1,28 @@
 #!/bin/bash
 set -euxo pipefail
 
-cd finish
 mvn -pl models clean install
 
 mvn -ntp -Dhttp.keepAlive=false \
     -Dmaven.wagon.http.pool=false \
     -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
-    -pl system clean package liberty:create liberty:install-feature liberty:deploy
+    -pl system -q clean package liberty:create liberty:install-feature liberty:deploy
 
 mvn -ntp -Dhttp.keepAlive=false \
     -Dmaven.wagon.http.pool=false \
     -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
-    -pl inventory clean package liberty:create liberty:install-feature liberty:deploy
+    -pl inventory -q clean package liberty:create liberty:install-feature liberty:deploy
 
-mvn -ntp -pl system liberty:start
 mvn -ntp -pl inventory liberty:start
+sleep 10
+mvn -ntp -pl system liberty:start
+sleep 15
 
-mvn -ntp -Dhttp.keepAlive=false \
-    -Dmaven.wagon.http.pool=false \
-    -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
-    -pl system failsafe:integration-test
 mvn -ntp -Dhttp.keepAlive=false \
     -Dmaven.wagon.http.pool=false \
     -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
     -pl inventory failsafe:integration-test
 
-mvn -ntp -pl system failsafe:verify
 mvn -ntp -pl inventory failsafe:verify
 
 mvn -pl system liberty:stop
